70a07efe22295618657719b2fe177ec6eac007e2
VictorKazankov <sadovnichi@mail.ru>
2020-07-08 14:33:59 +0300

    Add verify user registration by Selenium+PyTest
    - added test user registration to test_registration.py
    - added helper.py file for helpers function
    - added locators.py
    - set default value for locale in contest.py

---
+++ helpers.py
@@ -0,0 +1,14 @@
+import random
+
+
+def create_user_data():
+    num = int(random.random() * 10000)
+    mail_address = f"mymail{num}@mail.com"
+    password = "21071976#ED2ws"
+    return mail_address, password
+
+
+def fill_field(browser, locator, value):
+    email_field = browser.find_element_by_id(locator)
+    email_field.clear()
+    email_field.send_keys(value)
---
+++ locators.py
@@ -0,0 +1,7 @@
+
+LOGIN_LINK_ID = "login_link"
+EMAIL_REGISTRATION_FIELD_ID = "id_registration-email"
+PASSWORD_REGISTRATION_FIELD_ID = "id_registration-password1"
+PASSWORD_CONFIRM_REGISTRATION_FIELD_ID = "id_registration-password2"
+REGISTRATION_BUTTON_NAME = "registration_submit"
+CONFIRM_REGISTRATION_ALERT_NAME = "alert-success"
