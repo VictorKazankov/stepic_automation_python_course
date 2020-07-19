24fa57a2eca5271784616ebf936b84c32059d1c2
VictorKazankov <sadovnichi@mail.ru>
2020-07-19 03:04:37 +0300

    Add final project
    - create structure project
    - add conftest file with run browser
    - add tests files
    - add pages folder

---
+++ final_project/conftest.py
@@ -0,0 +1,49 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+link_general = 'http://selenium1py.pythonanywhere.com/'
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--language", action="store", default="ru")
+
+
+def get_language_local_chrome(request):
+    local = request.config.getoption("--language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': local})
+    return options
+
+
+def get_language_local_firefox(request):
+    local = request.config.getoption("--language")
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", local)
+    return fp
+
+
+def change_browser(request):
+    browser_name = request.config.getoption("--browser")
+    if browser_name == "chrome":
+        br = webdriver.Chrome(options=get_language_local_chrome(request))
+    elif browser_name == "firefox":
+        br = webdriver.Firefox(firefox_profile=get_language_local_firefox(request))
+    else:
+        raise ValueError("Unrecognized browser {}".format(browser_name))
+    return br
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart chrome browser for test..")
+    browser = change_browser(request)
+    browser.implicitly_wait(20)
+    browser.get(link_general)
+    time.sleep(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ final_project/test_product_page.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    pass
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket():
+    pass
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page():
+    pass
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page():
+    pass
